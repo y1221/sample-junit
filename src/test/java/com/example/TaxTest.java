431d35994abf2846c08966c17beb223390c6a57e
okayu <yuto.gio.0515@gmail.com>
2024-12-20 17:30:59 +0900

    テストケース作成

--- src/test/java/com/example/TaxTest.java
+++ src/test/java/com/example/TaxTest.java
@@ -1,10 +1,19 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
 
 public class TaxTest {
+    private Tax tax;
+
+    @BeforeEach
+    void setUp() {
+        tax = new Tax();
+    }
+
     @Test
     void testTaxIn() {
-
+        assertEquals(1100, tax.taxIn(1000));
     }
 }
